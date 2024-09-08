40d44f98a5e5a68f7f2da00395afc4a4d440f910
Dhruv Pokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2024-09-08 09:47:54 +0530

    Create a function that generates random Computer choice
    It creates a global array that contains all the options of game, and also creates a function that generates a computer choice

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+// Create a global array that contains all the choices of rock, paper and scissors game
+// Create a get computer choice function 
+// Inside get computer choice function generate a random number from 0 to 2 (both inclusive)
+// Return a string(rock, paper, scissors) corresponding to the number 
+let choices = ["rock", "paper", "scissors"];
+function getComputerChoice(){
+    let randomChoice = Math.floor(Math.random()*3);
+    let compChoice = choices[randomChoice];
+    return compChoice;
+}
