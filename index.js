8244314703b184473856d4405f297d25736e11aa
Dhruv Pokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2024-09-09 11:33:55 +0530

    Shift the Pseudocode of the project to a new file

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 let CHOICES = ["rock", "paper", "scissors"];
 
-// Create a global array that contains all the choices of rock, paper and scissors game
-// Create a get computer choice function
-// Inside get computer choice function generate a random number from 0 to 2 (both inclusive)
-// Return a string(rock, paper, scissors) corresponding to the number
 function getComputerChoice() {
   let randomChoice = Math.floor(Math.random() * 3);
   let compChoice = CHOICES[randomChoice];
   return compChoice;
 }
-
-// Create a function that returns human Choice
-// Take input from user using prompt
-// Check whether the input is correct and return input if it a valid input
-// If it's an invalid input show an alert to user containing message that it's invalid input
 
 function getHumanChoice() {
   let humChoice = prompt(
@@ -26,17 +17,9 @@ function getHumanChoice() {
   }
 }
 
-// Create a playGame function that plays the entire game
-// It plays the game 5 times
-
 function playGame() {
-  // Initializing player scores variable with a value 0
-
   let humanScore = 0;
   let computerScore = 0;
-  // Create a function playRound
-  // It takes two parameter humanChoice and computerChoice
-  // It uses conditional to checks who wins and update the scores accordingly
 
   function playRound(humanChoice, computerChoice) {
     humanWin = false;
